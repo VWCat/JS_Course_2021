f4f085fc1a132a92dfedb071f12cb9fcf836b20b
VWCat <VWCat@mail.ru>
2021-09-25 12:38:09 +0300

    Second lesson class work commit

---
+++ Lesson_1/ClassWork/script.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+const ourPlanetName = "Earth";
+let currentUserTime = new Date();
+let articlesCount = 20;
+let isCashPayment = true;
+let lastName = "Пупкин";
+let firstName = "Василий";
+let middleName = "Иосифович";
+
+console.log(ourPlanetName);
+console.log(`${currentUserTime.getHours()}:${currentUserTime.getMinutes()}`);
+console.log(articlesCount);
+console.log(isCashPayment);
+console.log(lastName, firstName, middleName);
